cf17b3b2ac87625a6c082b76752bf4fcb06ed6a3
Lech Dulian <lech.dulian@capgemini.com>
2019-09-30 13:08:55 +0200

    restore one placeholder

--- inc/cookie-consent.php
+++ inc/cookie-consent.php
@@ -226,6 +226,14 @@ add_action(
 			</script>
 			<?php
 		endif;
+
+		if ( ! empty( $fm_cookie_fields['cookie-statistics-gtm'] ) ) :
+			?>
+			<script type='text/plain' data-name='cookie_statistics'>
+				(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','<?php echo esc_js( $fm_cookie_fields['cookie-statistics-gtm'] ); ?>');
+			</script>
+			<?php
+		endif;
 		?>
 
 		<script type='text/javascript'>
