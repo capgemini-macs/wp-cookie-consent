174c144811bb791ba983888c4031a024e785a478
Marcin Świrek <swirek.marcin@gmail.com>
2019-10-04 12:07:50 +0200

    Restore default content of files

--- assets/js/cg-cookie-methods.js
+++ assets/js/cg-cookie-methods.js
@@ -1,106 +1,74 @@
-"use strict";
-
 /**
  * Cookie get/set helper functions with user consent check
- *
+ * 
  * @author Remigiusz Loginow <remigiusz.loginow@capgemini.com>
  */
 
 /**
  * Cookie types
  */
-var cookieTypes = new Set(["necessary", "preferences", "statistics"]);
+const cookieTypes = new Set(['necessary', 'preferences', 'statistics'])
+
 /**
  * Check if cookie type is allowed
  * @param {string} cType cookie type
  */
-
-var allowCookie = function allowCookie(cType) {
+const allowCookie = (cType) => {
   // check if it's a proper type
   if (!cookieTypes.has(cType)) {
-    return false;
-  } // get allowed cookie types
-
-  var allowedCookies = getAllowedCookies(); // check if type is allowed
-
-  if (allowedCookies.has(cType)) {
-    return true;
+    return false
   }
 
-  return false;
-};
+  // get allowed cookie types
+  const allowedCookies = getAllowedCookies()
+  // check if type is allowed
+  if (allowedCookies.has(cType)) {
+    return true
+  }
+  return false
+}
+
 /**
  * Get types allowed by user
  */
-
-var getAllowedCookies = function getAllowedCookies() {
+const getAllowedCookies = () => {
   // return all types if user allowed all
-  if (getCookie("cookies_all") === "1") {
-    return cookieTypes;
-  } // search for individual types if user didn't allow all
-
-  var allowedCookieTypes = new Set();
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
-
-  try {
-    for (
-      var _iterator = cookieTypes[Symbol.iterator](), _step;
-      !(_iteratorNormalCompletion = (_step = _iterator.next()).done);
-      _iteratorNormalCompletion = true
-    ) {
-      var type = _step.value;
-
-      if (getCookie("cookie_".concat(type)) === "1") {
-        allowedCookieTypes.add(type);
-      }
-    }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator.return != null) {
-        _iterator.return();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
+  if (getCookie('cookies_all') === '1') {
+    return cookieTypes
+  }
+  // search for individual types if user didn't allow all
+  let allowedCookieTypes = new Set()
+  for (let type of cookieTypes) {
+    if (getCookie(`cookie_${type}`) === '1') {
+      allowedCookieTypes.add(type)
     }
   }
+  return allowedCookieTypes
+}
 
-  return allowedCookieTypes;
-};
 /**
  * Get cookie value
  * @param {string} cName cookie name
  */
-
-var getCookie = function getCookie(cName) {
-  var cValue = document.cookie;
-  var cStart = cValue.indexOf(" " + cName + "=");
-
+const getCookie = (cName) => {
+  let cValue = document.cookie
+  let cStart = cValue.indexOf(' ' + cName + '=')
   if (cStart === -1) {
-    cStart = cValue.indexOf(cName + "=");
+    cStart = cValue.indexOf(cName + '=')
   }
-
   if (cStart === -1) {
-    cValue = null;
+    cValue = null
   } else {
-    cStart = cValue.indexOf("=", cStart) + 1;
-    var cEnd = cValue.indexOf(";", cStart);
-
+    cStart = cValue.indexOf('=', cStart) + 1
+    let cEnd = cValue.indexOf(';', cStart)
     if (cEnd === -1) {
-      cEnd = cValue.length;
+      cEnd = cValue.length
     }
-
-    cValue = unescape(cValue.substring(cStart, cEnd));
+    cValue = unescape(cValue.substring(cStart, cEnd))
   }
+  return cValue
+}
 
-  return cValue;
-};
 /**
  * Set cookie
  * @param {string} cType cookie type
@@ -108,22 +76,19 @@ var getCookie = function getCookie(cName) {
  * @param {any} value cookie value
  * @param {number} exdays expiry after n days
  */
-
-var setCookie = function setCookie(cType, cName, value, exdays) {
+const setCookie = (cType, cName, value, exdays) => {
   if (!allowCookie(cType)) {
-    return;
+    return
   }
 
-  var exdate = new Date();
-  exdate.setDate(exdate.getDate() + exdays);
-  var cValue =
-    escape(value) +
-    (exdays === null ? "" : "; expires=" + exdate.toUTCString());
-  document.cookie = cName + "=" + cValue;
-};
+  const exdate = new Date()
+  exdate.setDate(exdate.getDate() + exdays)
+  const cValue = escape(value) + ((exdays === null) ? '' : '; expires=' + exdate.toUTCString())
+  document.cookie = cName + '=' + cValue
+}
 
 module.exports = {
-  allowCookie: allowCookie,
-  getCookie: getCookie,
-  setCookie: setCookie
-};
+  allowCookie,
+  getCookie,
+  setCookie
+}
--- assets/js/cg-cookie-yt.js
+++ assets/js/cg-cookie-yt.js
@@ -1,80 +1,40 @@
-"use strict";
-
-var _cgCookieMethods = require("./cg-cookie-methods");
-
 /**
  * Embed YT player with or without cookies if user allowed 'statistics' cookies
- *
- * Usage:
+ * 
+ * Usage: 
  * add HTML element with data-yt="{video id}" e.g.: <div class="iframe-wraper" data-yt="123456"></div>
  * import module and execute embedYTPlayers() function
- *
+ * 
  * @author Remigiusz Loginow <remigiusz.loginow@capgemini.com>
  */
-var embedYTPlayers = function embedYTPlayers() {
-  var allow = (0, _cgCookieMethods.allowCookie)("statistics");
-  ready(function() {
-    var players = document.querySelectorAll("[data-yt]");
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
 
-    try {
-      for (
-        var _iterator = players[Symbol.iterator](), _step;
-        !(_iteratorNormalCompletion = (_step = _iterator.next()).done);
-        _iteratorNormalCompletion = true
-      ) {
-        var player = _step.value;
-        var id = player.getAttribute("data-yt");
-        var url = void 0;
+import { allowCookie } from './cg-cookie-methods'
 
-        if (allow) {
-          url = "https://www.youtube.com/embed/".concat(
-            id,
-            "?autoplay=0&rel=0"
-          );
-        } else {
-          url = "https://www.youtube-nocookie.com/embed/".concat(
-            id,
-            "?autoplay=0&rel=0"
-          );
-        }
-
-        player.innerHTML = '<iframe class="video_yt" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="'.concat(
-          url,
-          '" frameborder="0" allowfullscreen></iframe>'
-        );
+const embedYTPlayers = () => {
+  const allow = allowCookie('statistics')
+  ready(() => {
+    const players = document.querySelectorAll('[data-yt]')
+    for (const player of players) {
+      const id = player.getAttribute('data-yt')
+      let url
+      if (allow) {
+        url = `https://www.youtube.com/embed/${id}?autoplay=0&rel=0`
+      } else {
+        url = `https://www.youtube-nocookie.com/embed/${id}?autoplay=0&rel=0`
       }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
-      try {
-        if (!_iteratorNormalCompletion && _iterator.return != null) {
-          _iterator.return();
-        }
-      } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
-        }
-      }
+      player.innerHTML = `<iframe class="video_yt" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" type="text/html" src="${url}" frameborder="0" allowfullscreen></iframe>`
     }
-  });
-};
+  })
+}
 
-var ready = function ready(fn) {
-  if (
-    document.attachEvent
-      ? document.readyState === "complete"
-      : document.readyState !== "loading"
-  ) {
-    fn();
+const ready = (fn) => {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn()
   } else {
-    document.addEventListener("DOMContentLoaded", fn);
+    document.addEventListener('DOMContentLoaded', fn)
   }
-};
+}
 
 module.exports = {
-  embedYTPlayers: embedYTPlayers
-};
+  embedYTPlayers
+}
